a105f0ac3897138e7cb9a89facd34b82c77df494
Hugo Guerra <guerrahugoa@gmail.com>
2017-01-23 13:40:50 -0800

    added route files, added router and route handling to serverjs

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
@@ -5,6 +7,19 @@ const bodyParser = require('body-parser');
 
 const DB_PORT = process.env.MONGOLAB_URI || 'mongodb://localhost/db';
 mongoose.connect(DB_PORT);
+
+let publicRouter = express.Router();
+let apiRouter = express.Router();
+let loginRouter = express.Router();
+
+require('./routes/flowers-routes')(apiRouter);
+require('./routes/growers-routes')(apiRouter);
+require('./routes/sellers-routes')(apiRouter);
+require('./routes/badges-routes')(apiRouter);
+
+require('./routes/events-routes')(apiRouter);
+require('./routes/users-routes')(loginRouter);
+require('./routes/login-routes')(apiRouter);
 
 app.use(bodyParser.json());
 
