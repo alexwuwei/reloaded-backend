dded3287c015b8b92e5fd51a39d86010a2c6c188
Hugo Guerra <guerrahugoa@gmail.com>
2017-01-23 13:21:01 -0800

    adds mongo paths and model files

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const DB_PORT = process.env.MONGOLAB_URI || 'mongodb://localhost/db';
+mongoose.connect(DB_PORT);
+
 app.use(bodyParser.json());
 
 app.listen(3000, () => {
